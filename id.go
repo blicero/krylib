af670db1835d89641aaf9f26cadbca2c16e4b949
Benjamin Walkenhorst <krylon@posteo.de>
2022-10-25 00:59:04 +0200

    Reintroduce type ID for ... reasons

---
+++ id.go
@@ -0,0 +1,10 @@
+// /home/krylon/go/src/github.com/blicero/krylib/id.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 25. 10. 2022 by Benjamin Walkenhorst
+// (c) 2022 Benjamin Walkenhorst
+// Time-stamp: <2022-10-25 00:57:12 krylon>
+
+package krylib
+
+// ID is an identifier that is unique within a given context.
+type ID int64
