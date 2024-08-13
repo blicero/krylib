dddff3d6c9c9916d69a646333ce3542793539dd5
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-13 20:44:06 +0200

    Remove duplicate definition of interface Number

--- number.go
+++ number.go
@@ -2,11 +2,11 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 14. 06. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-06-14 21:23:08 krylon>
+// Time-stamp: <2024-08-13 20:44:00 krylon>
 
 package krylib
 
 // Number is a generic type that can be either a real or an integer.
-type Number interface {
-	int64 | float64
-}
+// type Number interface {
+// 	int64 | float64
+// }
