0c78d5b6060b4bd3d5929f3b381ae0405e3521a8
Benjamin Walkenhorst <krylon@posteo.de>
2026-06-27 11:55:27 +0200

    add ErrNil, fix spelling in comments

--- errors.go
+++ errors.go
@@ -2,17 +2,20 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 13. 09. 2019 by Benjamin Walkenhorst
 // (c) 2019 Benjamin Walkenhorst
-// Time-stamp: <2019-09-13 21:45:24 krylon>
+// Time-stamp: <2026-06-27 11:52:59 krylon>
 
 package krylib
 
 import "errors"
 
-// ErrNotImplemented Indicates that some functionality is not implemented, yet.
+// ErrNotImplemented indicates that some functionality is not implemented, yet.
 var ErrNotImplemented error = errors.New("not implemented") // nolint: golint
 
-// ErrInvalidValue Indicates that some value is invalid.
+// ErrInvalidValue indicates that some value is invalid.
 var ErrInvalidValue error = errors.New("invalid value")
+
+// ErrNil indicates a parameter is nil that is not supposed to be.
+var ErrNil error = errors.New("argument is nil")
 
 // Local Variables:  //
 // compile-command: "go generate && go vet && go build -v -p 16 && gometalinter && go test -v" //
